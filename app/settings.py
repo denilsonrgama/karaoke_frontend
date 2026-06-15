3c750fe91a038d6d6dea7d7925e7e90b906d5d9a
DenilsonGama <drogama@outlook.com>
2026-06-15 12:12:24 -0300

    Apply saloon theme across app screens

--- app/settings.py
+++ app/settings.py
@@ -79,7 +79,7 @@ ROOT_URLCONF = 'app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        "DIRS": [BASE_DIR / "templates"],
+        "DIRS": [BASE_DIR / "templates", BASE_DIR / "accounts" / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
